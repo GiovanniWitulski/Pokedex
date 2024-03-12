b33415e6c065a87a9590ff84f822ee0a83db04dd
Giovanni Witulski <witulski.schule@gmail.com>
2024-03-12 17:19:17 +0100

    added some html + pokemon species

--- pokedex.js
+++ pokedex.js
@@ -1,16 +1,24 @@
 let currentPokemon;
+let currentPokemonSpecies;
 
 function init() {
 
 }
 
 async function loadPokemon() {
-    let url = 'https://pokeapi.co/api/v2/pokemon/charmander';
+    let url = 'https://pokeapi.co/api/v2/pokemon/4';
     let response = await fetch(url);
     currentPokemon = await response.json();
     console.log('loadet pokemon', currentPokemon);
-
+    await loadPokemonSpecies();
     renderPokemonInfo();
+}
+
+async function loadPokemonSpecies() {
+    let url = 'https://pokeapi.co/api/v2/pokemon-species/4/';
+    let response = await fetch(url);
+    currentPokemonSpecies = await response.json();
+    console.log('loadet pokemon species', currentPokemonSpecies);
 }
 
 function renderPokemonInfo() {
@@ -22,6 +30,8 @@ function renderPokemonInfo() {
     let formattedType1 = currentPokemon['types']['0']['type']['name'].charAt(0).toUpperCase() + currentPokemon['types']['0']['type']['name'].slice(1);
     document.getElementById('pokemonType1').innerHTML = formattedType1;
 
-    let formattedId = currentPokemon['id'].toString().padStart(3, '0');
+    let formattedId = currentPokemon['id'].toString().padStart(4, '0');
     document.getElementById('pokemonId').innerHTML = '#' + formattedId;
+
+    document.getElementById('pokemonSpecies').innerHTML = currentPokemonSpecies['genera']['7']['genus'];
 }
